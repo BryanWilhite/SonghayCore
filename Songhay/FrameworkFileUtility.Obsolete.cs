155119e84ba6abb0525376b099d55c2a102ffb0f
Bryan Wilhite <bryan.wilhite@gmail.com>
2016-09-21 11:52:39 -0700

    FrameworkFileUtility re-org

--- Songhay/FrameworkFileUtility.Obsolete.cs
+++ Songhay/FrameworkFileUtility.Obsolete.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 
 namespace Songhay
 {
@@ -78,33 +77,6 @@ namespace Songhay
             string ret = GetFileName(pathWithFile, pathDelimiter);
             return pathWithFile.Replace(ret, string.Empty);
         }
-
-#if !SILVERLIGHT
-        /// <summary>
-        /// Gets the UTF-8 encoded string.
-        /// </summary>
-        /// <param name="rawValue">The raw value.</param>
-        public static string GetEncodedString(string rawValue)
-        {
-            return FrameworkFileUtility.GetEncodedString(rawValue, Encoding.UTF8);
-        }
-
-        /// <summary>
-        /// Gets the encoded string.
-        /// </summary>
-        /// <param name="rawValue">The raw value (<see cref="System.Text.Encoding.ASCII"/> by default).</param>
-        /// <param name="encoding">The encoding.</param>
-        public static string GetEncodedString(string rawValue, Encoding encoding)
-        {
-            if(encoding == null)
-                throw new ArgumentNullException("encoding",
-                    "The expected encoding is not here.");
-
-            byte[] b = Encoding.Convert(Encoding.ASCII, encoding, encoding.GetBytes(rawValue));
-
-            return new string(Encoding.ASCII.GetChars(b));
-        }
-#endif
 
         /// <summary>
         /// Joins the path and root.
