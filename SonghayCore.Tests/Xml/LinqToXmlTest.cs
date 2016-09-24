0c659dcd6935cdca15e14142029205ce2b1c6d91
Bryan Wilhite <rasx@songhaysystem.com>
2016-09-23 22:18:59 -0700

    removed more non-covering tests to LinqPad

--- SonghayCore.Tests/Xml/LinqToXmlTest.cs
+++ SonghayCore.Tests/Xml/LinqToXmlTest.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Songhay.Extensions;
 using Songhay.Xml;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Songhay.Tests
@@ -15,20 +11,6 @@ namespace Songhay.Tests
     [TestClass]
     public class LinqToXmlTest
     {
-        [TestInitialize]
-        public void ClearPreviousTestResults()
-        {
-            var directory = Directory.GetParent(TestContext.TestDir);
-
-            directory.GetFiles()
-                .OrderByDescending(f => f.LastAccessTime).Skip(1)
-                .ForEachInEnumerable(f => f.Delete());
-
-            directory.GetDirectories()
-                .OrderByDescending(d => d.LastAccessTime).Skip(1)
-                .ForEachInEnumerable(d => d.Delete(true));
-        }
-
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
@@ -67,49 +49,6 @@ namespace Songhay.Tests
 
             var attr = c.FirstOrDefault().Attribute("foo");
             Assert.IsNull(attr);
-        }
-
-        [TestMethod]
-        [TestProperty("uri", "http://www.weather.gov/xml/current_obs/KLAX.xml")]
-        public async Task ShouldGetRemoteXmlAsync()
-        {
-            var uri = this.TestContext.Properties["uri"].ToString();
-
-            using (var client = new HttpClient())
-            {
-                var result = await client.GetStringAsync(uri);
-                var remoteDocument = XDocument.Parse(result);
-                var isCorrectAssertion = (remoteDocument.Root.Name == "current_observation");
-                Assert.IsTrue(isCorrectAssertion, "The expected Root Name is not here.");
-            }
-        }
-
-        [TestMethod]
-        [Description("Should use empty namespaces.")]
-        public void ShouldUseEmptyNamespaces()
-        {
-            var xml = @"
-<root>
-    <a>
-        <b>
-            <c>a text node</c>
-        </b>
-    </a>
-</root>
-";
-            var xd = XDocument.Parse(xml);
-
-            XNamespace empty = string.Empty;
-
-            var c = xd
-                .Elements(empty + "root")
-                .Elements(empty + "a")
-                .Elements(empty + "b")
-                .Elements(empty + "c");
-            Assert.IsNotNull(c);
-
-            Assert.AreEqual(c.FirstOrDefault(),
-                xd.Descendants(empty + "c").FirstOrDefault());
         }
     }
 }
--- SonghayCore.Tests/Xml/XObjectUtilityTest.cs
+++ SonghayCore.Tests/Xml/XObjectUtilityTest.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Linq;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Xml;
 using System.Xml.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Songhay.Tests
 {
     /// <summary>
-    /// Defines tests for <see cref="Songhay.Xml.XObjectUtility"/>.
+    /// Defines tests for <see cref="Xml.XObjectUtility"/>.
     /// </summary>
     [TestClass]
     public class XObjectUtilityTest
@@ -17,52 +16,24 @@ namespace Songhay.Tests
         ///</summary>
         public TestContext TestContext { get; set; }
 
-        /// <summary>
-        /// Should clone the element.
-        /// </summary>
-        /// <remarks>
-        /// See “Empty Elements and Self-Closing Tags”
-        /// (http://blogs.msdn.com/b/ericwhite/archive/2009/07/08/empty-elements-and-self-closing-tags.aspx)
-        /// </remarks>
         [TestMethod]
-        [Description("Should clone element.")]
-        public void ShouldCloneElement()
+        [TestProperty("expectedValue", "one-text-node-child-at-the-end")]
+        [TestProperty("sampleOne", "<root><one>one-text-node<one-child>-child</one-child>-at-the-end</one></root>")]
+        public void ShouldJoinFlattenedXTextNodes()
         {
-            XElement root = XElement.Parse("<Root></Root>");
-            TestContext.WriteLine("Original tree: {0}", root);
-            XElement rootClone = CloneElement(root);
-            TestContext.WriteLine("Cloned tree: {0}", rootClone);
-            Assert.AreEqual("<Root />", rootClone.ToString(), "Default clone result not equal.");
-            XElement rootCloneWithEmptyTags = CloneElement(root, renderEmptyTags: true);
-            TestContext.WriteLine("Cloned tree with empty tags: {0}", rootCloneWithEmptyTags);
-            Assert.AreEqual(root.ToString(), rootCloneWithEmptyTags.ToString(), "Clone result with empty tags not equal.");
-        }
+            #region test properties:
 
-        [TestMethod]
-        [Description("Should write outer XML.")]
-        public void ShouldWriteOuterXml()
-        {
-            var e = new XElement("parent", new XElement("child"));
-            TestContext.WriteLine(e.ToString());
-        }
+            var expectedValue = this.TestContext.Properties["expectedValue"].ToString();
+            var sampleOne = this.TestContext.Properties["sampleOne"].ToString();
 
-        static XElement CloneElement(XElement element, bool renderEmptyTags = false)
-        {
-            var isEmptyElement = new Func<XElement, bool>((xe) =>
-            {
-                return !xe.IsEmpty && !xe.Nodes().OfType<XText>().Any();
-            });
+            #endregion
 
-            return new XElement(element.Name,
-                element.Attributes(),
-                element.Nodes().Select(n =>
-                {
-                    XElement e = n as XElement;
-                    if(e != null) return CloneElement(e, renderEmptyTags);
-                    return n;
-                }),
-                (isEmptyElement.Invoke(element) && renderEmptyTags) ? string.Empty : null
-            );
+            var rootElement = XElement.Parse(sampleOne);
+            this.TestContext.WriteLine(rootElement.ToString());
+
+            var actual = XObjectUtility.JoinFlattenedXTextNodes(rootElement);
+            this.TestContext.WriteLine("actual: {0}", actual);
+            Assert.AreEqual(expectedValue, actual, "The expected value is not here.");
         }
     }
 }
