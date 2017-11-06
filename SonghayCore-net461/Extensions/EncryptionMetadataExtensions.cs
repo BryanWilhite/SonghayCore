bc348e581641eb3c0fbd722ca9d7349f0ccaeb17
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-06 14:23:24 -0800

    :tractor: re-org

---
+++ SonghayCore-net461/Extensions/EncryptionMetadataExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Songhay.Models;
+using Songhay.Security;
+using System;
+using System.Configuration;
+
+namespace Songhay.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="EncryptionMetadata"/>
+    /// </summary>
+    public static class EncryptionMetadataExtensions
+    {
+        /// <summary>
+        /// Decrypts the specified encrypted string.
+        /// </summary>
+        /// <param name="encryptionMeta">The encryption meta.</param>
+        /// <param name="encryptedString">The encrypted string.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// encryptionMeta;The expected metadata is not here.
+        /// or
+        /// encryptedString;The expected encrypted string is not here.
+        /// </exception>
+        public static string Decrypt(this EncryptionMetadata encryptionMeta, string encryptedString)
+        {
+            if (encryptionMeta == null) throw new ArgumentNullException("encryptionMeta", "The expected metadata is not here.");
+            if (string.IsNullOrEmpty(encryptedString)) throw new ArgumentNullException("encryptedString", "The expected encrypted string is not here.");
+
+            var crypt = new SymmetricCrypt();
+            return crypt.Decrypt(encryptedString, encryptionMeta.Key, encryptionMeta.InitialVector);
+        }
+
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
+        /// <param name="encryptionMeta">The encryption meta.</param>
+        /// <param name="settings">The settings.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// encryptionMeta;The expected metadata is not here.
+        /// or
+        /// settings;The expected configuration settings are not here.
+        /// </exception>
+        public static string GetConnectionString(this EncryptionMetadata encryptionMeta, ConnectionStringSettings settings)
+        {
+            if (settings == null) throw new ArgumentNullException("settings", "The expected configuration settings are not here.");
+
+            var connectionString = encryptionMeta.Decrypt(settings.ConnectionString);
+            return connectionString;
+        }
+    }
+}
---
+++ SonghayCore-net461/Models/ISyndicatable.cs
@@ -0,0 +1,22 @@
+﻿#if !NET35
+using System.ServiceModel.Syndication;
+
+namespace Songhay.Models
+{
+    /// <summary>
+    /// Specifies that a Model supports syndication.
+    /// </summary>
+    public interface ISyndicatable
+    {
+        /// <summary>
+        /// Gets the syndication feed.
+        /// </summary>
+        SyndicationFeed GetSyndicationFeed();
+
+        /// <summary>
+        /// Gets the syndication item.
+        /// </summary>
+        SyndicationItem GetSyndicationItem();
+    }
+}
+#endif
