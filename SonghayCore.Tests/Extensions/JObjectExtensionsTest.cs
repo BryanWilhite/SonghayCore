18cabd38537b461daf1da8900a5e8fed471ec748
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-07 13:22:44 -0800

    :tractor: SonghayCore.Tests added (for .NET Standard 2.0)

---
+++ SonghayCore.Tests/Extensions/JObjectExtensionsTest.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+
+namespace Songhay.Extensions.Tests
+{
+
+    /// <summary>
+    /// Tests for extensions of <see cref="System.DateTime"/>.
+    /// </summary>
+    [TestClass]
+    public class JObjectExtensionsTest
+    {
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestProperty("json", @"{ ""data"": { ""one"":""uno"", ""two"":""dos"" } }")]
+        public void ShouldGetDictionaryOfStrings()
+        {
+            var json = this.TestContext.Properties["json"].ToString();
+            this.TestContext.WriteLine("json: {0}", json);
+
+            var jO = JObject.Parse(json);
+
+            var data = jO.GetDictionary("data", throwException: true);
+            this.TestContext.WriteLine("Dictionary keys: {0}", string.Join(",", data.Keys.ToArray()));
+        }
+
+        [TestMethod]
+        [TestProperty("json", @"{ ""one"":[""uno"", ""un""], ""two"":[""dos"", ""deux""] }")]
+        public void ShouldGetDictionaryOfArrayOfStrings()
+        {
+            var json = this.TestContext.Properties["json"].ToString();
+            this.TestContext.WriteLine("json: {0}", json);
+
+            var jO = JObject.Parse(json);
+
+            var data = jO.GetDictionary(throwException: true);
+            this.TestContext.WriteLine("Dictionary keys: {0}", string.Join(",", data.Keys.ToArray()));
+        }
+
+        [TestMethod]
+        [TestProperty("json", @"{ ""items"": [ ""one"", ""two"", ""three"" ] }")]
+        public void ShouldGetJArray()
+        {
+            var json = this.TestContext.Properties["json"].ToString();
+            this.TestContext.WriteLine("json: {0}", json);
+
+            var jO = JObject.Parse(json);
+
+            var jA = jO.GetJArray("items", throwException: true);
+            var jA_clone = jA.DeepClone();
+
+            jA.Add("four");
+            this.TestContext.WriteLine("original JArray: {0}", jA.ToString());
+            var e = jA.ElementAtOrDefault(3);
+            Assert.IsNotNull(e, "The expected 4th element is not here.");
+
+            this.TestContext.WriteLine("cloned JArray: {0}", jA_clone.ToString());
+            var e_clone = jA_clone.ElementAtOrDefault(3);
+            Assert.IsNull(e_clone, "The 4th element is not expected.");
+        }
+
+        [TestMethod]
+        [TestProperty("arrayIndex", "1")]
+        [TestProperty("expectedValue", "2")]
+        [TestProperty("json", @"{ ""items"": [ { ""x"": 1 }, { ""x"": 2 }, { ""x"": 3 } ] }")]
+        public void ShouldGetJTokenFromJArray()
+        {
+            #region test properties:
+
+            var arrayIndex = Convert.ToInt32(this.TestContext.Properties["arrayIndex"]);
+            var expectedValue = Convert.ToInt32(this.TestContext.Properties["expectedValue"]);
+            var json = this.TestContext.Properties["json"].ToString();
+
+            #endregion
+
+            this.TestContext.WriteLine("arrayIndex: {0}", arrayIndex);
+            this.TestContext.WriteLine("expectedValue: {0}", expectedValue);
+            this.TestContext.WriteLine("json: {0}", json);
+
+            var jO = JObject.Parse(json);
+
+            var token = jO.GetJTokenFromJArray("items", "x", arrayIndex, throwException: true);
+            Assert.IsNotNull((token as JValue), "The expected JValue is not here.");
+
+            var actualValue = (token as JValue).Value<int>();
+            this.TestContext.WriteLine("actualValue: {0}", actualValue);
+            Assert.AreEqual(expectedValue, actualValue, "The expected JObject value is not here.");
+        }
+
+        [TestMethod]
+        [TestProperty("json", @"{ ""items"": [], ""otherItems"": null }")]
+        public void ShouldNotGetJArray()
+        {
+            var json = this.TestContext.Properties["json"].ToString();
+            this.TestContext.WriteLine("json: {0}", json);
+
+            var jO = JObject.Parse(json);
+
+            var jA = jO.GetJArray("items", throwException: false);
+            Assert.IsNull(jA, "JArray is not expected.");
+
+            var jA_other = jO.GetJArray("otherItems", throwException: false);
+            Assert.IsNull(jA_other, "JArray is not expected.");
+
+            var jA_otherOther = jO.GetJArray("otherOtherItems", throwException: false);
+            Assert.IsNull(jA_other, "JArray is not expected.");
+        }
+    }
+}
---
+++ SonghayCore.Tests/Models/OpmlTest.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Extensions;
+using Songhay.Xml;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Songhay.Tests
+{
+    /// <summary>
+    /// Summary description for OpmlTest
+    /// </summary>
+    [TestClass]
+    public class OpmlTest
+    {
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestProperty("opmlFile", @"Models\OpmlFromInfoPath.xml")]
+        public void ShouldFilterCategory()
+        {
+            var projectFolder = this.TestContext
+                .ShouldGetAssemblyDirectoryInfo(this.GetType())
+                .Parent
+                .Parent
+                .FullName;
+
+            var opmlFile = this.TestContext.Properties["opmlFile"].ToString();
+
+            var path = Path.Combine(projectFolder, opmlFile);
+            this.TestContext.ShouldFindFile(path);
+
+            var data = OpmlUtility.GetDocument(path);
+
+            Assert.IsTrue(data.OpmlBody.Outlines.Length == 4);
+
+            data.OpmlBody.Outlines = data.OpmlBody.Outlines
+                .Where(o => o.Category != "private").ToArray();
+
+            Assert.IsTrue(data.OpmlBody.Outlines.Length == 3);
+        }
+
+        [TestMethod]
+        [TestProperty("opmlFile", @"Models\OpmlTest.opml")]
+        public void ShouldLoadCategoriesAndResources()
+        {
+            var projectFolder = this.TestContext
+                .ShouldGetAssemblyDirectoryInfo(this.GetType())
+                .Parent
+                .Parent
+                .FullName;
+
+            var opmlFile = this.TestContext.Properties["opmlFile"].ToString();
+
+            var path = Path.Combine(projectFolder, opmlFile);
+            this.TestContext.ShouldFindFile(path);
+
+            var data = OpmlUtility.GetDocument(path);
+
+            //XPATH: ./outline[not(@url)]
+            var categories = data.OpmlBody.Outlines
+                .Where(outline => outline.Url == null);
+
+            categories.ForEachInEnumerable(category =>
+                TestContext.WriteLine("Category: " + category.Text));
+
+            //XPATH./outline[@type="link"]
+            var resources = categories.First().Outlines
+                .Where(outline => outline.OutlineType == "link");
+
+            resources.ForEachInEnumerable(resource =>
+                TestContext.WriteLine("Resource: " + resource.Text));
+        }
+
+        [TestMethod]
+        [TestProperty("opmlFile", @"Models\OpmlFromInfoPath.xml")]
+        public void ShouldLoadDocument()
+        {
+            var projectFolder = this.TestContext
+                .ShouldGetAssemblyDirectoryInfo(this.GetType())
+                .Parent
+                .Parent
+                .FullName;
+
+            var opmlFile = this.TestContext.Properties["opmlFile"].ToString();
+
+            var path = Path.Combine(projectFolder, opmlFile);
+            this.TestContext.ShouldFindFile(path);
+
+            var data = OpmlUtility.GetDocument(path);
+
+            var expected = "Development Server";
+            var actual = data.OpmlHead.Title;
+            Assert.AreEqual(expected, actual);
+
+            expected = "LINQ to Entities Paging";
+            actual = data.OpmlBody.Outlines
+                .Where(o => o.Text == "Samples")
+                .First().Outlines.First().Text;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestProperty("opmlFile", @"Models\OpmlTest.opml")]
+        public void ShouldWriteDateModified()
+        {
+            var projectFolder = this.TestContext
+                .ShouldGetAssemblyDirectoryInfo(this.GetType())
+                .Parent
+                .Parent
+                .FullName;
+
+            var opmlFile = this.TestContext.Properties["opmlFile"].ToString();
+
+            var path = Path.Combine(projectFolder, opmlFile);
+            this.TestContext.ShouldFindFile(path);
+
+            var data = OpmlUtility.GetDocument(path);
+            DateTime? date = DateTime.Now;
+            data.OpmlHead.DateModified = date;
+            XmlUtility.Write(data, path);
+
+            data = OpmlUtility.GetDocument(path);
+            var actualDate = data.OpmlHead.DateModified;
+            Assert.AreEqual(date.Value.Day, actualDate.Value.Day);
+            Assert.AreEqual(date.Value.Hour, actualDate.Value.Hour);
+            Assert.AreEqual(date.Value.Minute, actualDate.Value.Minute);
+        }
+    }
+}
