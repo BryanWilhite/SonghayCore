9acf65aef758d2f00ce324b4eaa6ffbb0fe7a4e5
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-06 23:30:06 -0800

    :umbrella: PlatformSupportTests added

--- Songhay.Tests/PlatformSupportTests.cs
+++ Songhay.Tests/PlatformSupportTests.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Extensions;
 
 namespace Songhay.Tests
 {
     [TestClass]
     public class PlatformSupportTests
     {
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void ShouldFindMisswingCompileIncludes()
         {
+            var projectsFolder = this.TestContext.ShouldGetAssemblyDirectoryParent(this.GetType(), expectedLevels: 3);
         }
     }
 }
