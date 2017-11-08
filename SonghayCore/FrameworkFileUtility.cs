746b3d02cf966519a634dc81c1695e06354a5ac2
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-07 17:30:10 -0800

    :tractor: :hammer: FrameworkFileUtility split

--- SonghayCore/FrameworkFileUtility.cs
+++ SonghayCore/FrameworkFileUtility.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Songhay
@@ -76,33 +75,6 @@ namespace Songhay
 
             return FindParentDirectoryInfo(info.Parent.FullName, parentName, levels);
         }
-
-#if !SILVERLIGHT
-        /// <summary>
-        /// Gets the UTF-8 encoded string.
-        /// </summary>
-        /// <param name="rawValue">The raw value.</param>
-        public static string GetEncodedString(string rawValue)
-        {
-            return FrameworkFileUtility.GetEncodedString(rawValue, Encoding.UTF8);
-        }
-
-        /// <summary>
-        /// Gets the encoded string.
-        /// </summary>
-        /// <param name="rawValue">The raw value (<see cref="System.Text.Encoding.ASCII"/> by default).</param>
-        /// <param name="encoding">The encoding.</param>
-        public static string GetEncodedString(string rawValue, Encoding encoding)
-        {
-            if (encoding == null)
-                throw new ArgumentNullException("encoding",
-                    "The expected encoding is not here.");
-
-            byte[] b = Encoding.Convert(Encoding.ASCII, encoding, encoding.GetBytes(rawValue));
-
-            return new string(Encoding.ASCII.GetChars(b));
-        }
-#endif
 
         /// <summary>
         /// Gets the parent directory.
