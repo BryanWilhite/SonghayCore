b76706eec5df6e5c557c792fc18eb23e292fa66a
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-07 17:09:47 -0800

    :tractor: :hammer: ObjectExtensions split

--- SonghayCore/Extensions/ObjectExtensions.cs
+++ SonghayCore/Extensions/ObjectExtensions.cs
@@ -1,60 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Reflection;
 
 namespace Songhay.Extensions
 {
     /// <summary>
-    /// Extensions of <see cref="System.Object"/>.
+    /// Extensions of <see cref="object"/>.
     /// </summary>
-    public static class ObjectExtensions
+    public static partial class ObjectExtensions
     {
-        /// <summary>
-        /// Gets the properties.
-        /// </summary>
-        /// <param name="objectWithProperties">The object with properties.</param>
-        public static PropertyInfo[] GetProperties(this object objectWithProperties)
-        {
-            if (objectWithProperties == null) return Enumerable.Empty<PropertyInfo>().ToArray();
-            return objectWithProperties.GetType().GetProperties();
-        }
-
-        /// <summary>
-        /// Gets the property.
-        /// </summary>
-        /// <param name="objectWithProperties">The object with properties.</param>
-        /// <param name="propertyName">Name of the property.</param>
-        public static PropertyInfo GetProperty(this object objectWithProperties, string propertyName)
-        {
-            var props = objectWithProperties.GetProperties();
-            if (!props.Any()) return null;
-            return props.FirstOrDefault(i => i.Name == propertyName);
-        }
-
-#if !NETSTANDARD1_2 && !SILVERLIGHT && !PCL && !NET35 && !NET40
-
-        /// <summary>
-        /// Gets the property value.
-        /// </summary>
-        /// <param name="objectWithProperties">The object with properties.</param>
-        /// <param name="propertyName">Name of the property.</param>
-        /// <remarks>
-        /// Very useful for an MVVM situation like this:
-        /// <code>
-        ///     var command = this.DataContext.GetPropertyValue("MyCommand") as ICommand;
-        /// </code>
-        /// </remarks>
-        public static object GetPropertyValue(this object objectWithProperties, string propertyName)
-        {
-            var property = objectWithProperties.GetProperty(propertyName);
-            if (property == null) return null;
-            return property.GetValue(objectWithProperties);
-        }
-
-#endif
-
-#if !NETSTANDARD1_2 && !PCL
-
         /// <summary>
         /// Determines whether this instance is type.
         /// </summary>
@@ -66,13 +18,12 @@ namespace Songhay.Extensions
         }
 
         /// <summary>
-        /// Boxes the value in object or returns <see cref="System.DBNull"/>.
+        /// Boxes the value in object or returns <see cref="DBNull"/>.
         /// </summary>
         /// <param name="objectOfDomain">The object of domain.</param>
         public static object ToObjectOrDBNull(this object objectOfDomain)
         {
             return objectOfDomain ?? DBNull.Value;
         }
-#endif
     }
 }
