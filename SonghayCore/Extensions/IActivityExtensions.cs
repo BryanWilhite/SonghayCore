6d9cb1c43f27d7cf0a5229f103ecf241cb1e7485
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-08 15:04:51 -0800

    :tractor: multi-platform support chores

--- SonghayCore/Extensions/IActivityExtensions.cs
+++ SonghayCore/Extensions/IActivityExtensions.cs
@@ -1,37 +1,14 @@
-﻿using Songhay.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Songhay.Extensions
 {
     /// <summary>
-    /// Extensions of <see cref="IActivity"/>
+    /// Extensions of <see cref="Songhay.Models.IActivity"/>
     /// </summary>
-    public static class IActivityExtensions
+    public static partial class IActivityExtensions
     {
-        /// <summary>
-        /// Gets the activity.
-        /// </summary>
-        /// <param name="activities">The activities.</param>
-        /// <param name="activityName">Name of the activity.</param>
-        /// <returns></returns>
-        /// <exception cref="System.ArgumentNullException">
-        /// The expected Activity name is not here.
-        /// or
-        /// </exception>
-        public static IActivity GetActivity(this Dictionary<string, Lazy<IActivity>> activities, string activityName)
-        {
-            if (activities == null) return null;
-            if (string.IsNullOrEmpty(activityName)) throw new ArgumentNullException("The expected Activity name is not here.");
-            if (!activities.Keys.Contains(activityName)) throw new ArgumentNullException($"The expected Activity name, {activityName}, is not here.");
-
-            var activity = activities[activityName].Value;
-            if (activity == null) throw new NullReferenceException("The expected Activity is not here.");
-
-            return activity;
-        }
-
         /// <summary>
         /// Converts <c>args</c> to the name of the Activity.
         /// </summary>
