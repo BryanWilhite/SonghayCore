0f8b9a6265e8e1ad94486eaa5d98eb28ae13f760
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-07 18:15:33 -0800

    :umbrella: ShouldFindMissingCompileIncludes() started

---
+++ SonghayCore-net461.Tests/Models/PlatformConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Songhay.Tests.Models
+{
+    public static class PlatformConstants
+    {
+        public const string net35 = "net35";
+        public const string net40 = "net40";
+        public const string net451 = "net451";
+        public const string net452 = "net452";
+        public const string net461 = "net461";
+        public const string netstandard12 = "netstandard1.2";
+        public const string netstandard14 = "netstandard1.4";
+        public const string netstandard20 = "netstandard2.0";
+    }
+}
--- SonghayCore-net461.Tests/PlatformSupportTests.cs
+++ SonghayCore-net461.Tests/PlatformSupportTests.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Songhay.Extensions;
+using Songhay.Tests.Models;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Songhay.Tests
 {
@@ -14,9 +17,36 @@ namespace Songhay.Tests
         public TestContext TestContext { get; set; }
 
         [TestMethod]
+        [TestProperty("projectFileNamespace", "http://schemas.microsoft.com/developer/msbuild/2003")]
+        [TestProperty("projectsDirectory", "SonghayCore")]
+        [TestProperty("rootPlatform", PlatformConstants.net461)]
+        [TestProperty("targetPlatform", PlatformConstants.netstandard20)]
         public void ShouldFindMissingCompileIncludes()
         {
-            var projectsFolder = this.TestContext.ShouldGetAssemblyDirectoryParent(this.GetType(), expectedLevels: 3);
+            #region test properties:
+
+            XNamespace projectFileNamespace = this.TestContext.Properties["projectFileNamespace"].ToString();
+            var projectsDirectory = this.TestContext.Properties["projectsDirectory"].ToString();
+            var rootPlatform = this.TestContext.Properties["rootPlatform"].ToString();
+            var targetPlatform = this.TestContext.Properties["targetPlatform"].ToString();
+
+            #endregion
+
+            var info = this.TestContext.ShouldGetAssemblyDirectoryInfo(this.GetType());
+            var parentInfo = info.Parent?.Parent?.Parent;
+            Assert.IsNotNull(parentInfo, "The expected parent Directory Info is not here.");
+            Assert.AreEqual(parentInfo.Name, projectsDirectory);
+
+            var baseProjectFile = Path.Combine(parentInfo.FullName,
+                $"{projectsDirectory}-{rootPlatform}",
+                $"{projectsDirectory}-{rootPlatform}.csproj");
+            this.TestContext.ShouldFindFile(baseProjectFile);
+
+            var baseProjectDoc = XDocument.Load(baseProjectFile);
+            var baseProjectCompiles = baseProjectDoc.Root.Descendants(projectFileNamespace + "Compile").ToArray();
+            Assert.IsTrue(baseProjectCompiles.Any(), "The expected Compile elements are not here.");
+            this.TestContext.WriteLine("Base Project Compile Elements:");
+            baseProjectCompiles.ForEachInEnumerable(i => this.TestContext.WriteLine(i.Attribute("Include").Value));
         }
     }
 }
