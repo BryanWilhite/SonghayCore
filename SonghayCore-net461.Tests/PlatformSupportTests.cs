53cc6692272492462573d657d84780b440d0eeb2
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-08 17:38:56 -0800

    :umbrella: ShouldDiffCompileIncludesBetweenCoreAndFramework() works as expected

--- SonghayCore-net461.Tests/PlatformSupportTests.cs
+++ SonghayCore-net461.Tests/PlatformSupportTests.cs
@@ -82,5 +82,76 @@ namespace Songhay.Tests
                 this.TestContext.WriteLine("No base-target exceptions found.");
             }
         }
+
+        [TestMethod]
+        [TestProperty("projectFileNamespace", "http://schemas.microsoft.com/developer/msbuild/2003")]
+        [TestProperty("projectsDirectory", "SonghayCore")]
+        public void ShouldDiffCompileIncludesBetweenCoreAndFramework()
+        {
+            #region test properties:
+
+            XNamespace projectFileNamespace = this.TestContext.Properties["projectFileNamespace"].ToString();
+            var projectsDirectory = this.TestContext.Properties["projectsDirectory"].ToString();
+
+            #endregion
+
+            var info = this.TestContext.ShouldGetAssemblyDirectoryInfo(this.GetType());
+            var parentInfo = info.Parent?.Parent?.Parent;
+            Assert.IsNotNull(parentInfo, "The expected parent Directory Info is not here.");
+            Assert.AreEqual(parentInfo.Name, projectsDirectory);
+
+            var coreProjectInfo = parentInfo.GetDirectories().First(i => i.Name == projectsDirectory);
+            var coreFiles = coreProjectInfo
+                .EnumerateFiles("*.cs", SearchOption.AllDirectories)
+                .Select(i => i.FullName.Replace(coreProjectInfo.Parent.FullName, "..").ToLowerInvariant());
+
+            //coreFiles.ForEachInEnumerable(i => this.TestContext.WriteLine(i));
+
+            var framework20ProjectFile = Path.Combine(parentInfo.FullName,
+                $"{projectsDirectory.Replace("Core", string.Empty)}",
+                $"{projectsDirectory.Replace("Core", string.Empty)}.csproj");
+            this.TestContext.ShouldFindFile(framework20ProjectFile);
+
+            var framework20ProjectDoc = XDocument.Load(framework20ProjectFile);
+            var framework20ProjectCompiles = framework20ProjectDoc.Root.Descendants(projectFileNamespace + "Compile").ToArray();
+            Assert.IsTrue(framework20ProjectCompiles.Any(), "The expected framework 2.0 Compile elements are not here.");
+
+            var frameworkProjectFile = Path.Combine(parentInfo.FullName,
+                $"{projectsDirectory}-{PlatformConstants.net461}",
+                $"{projectsDirectory}-{PlatformConstants.net461}.csproj");
+            this.TestContext.ShouldFindFile(frameworkProjectFile);
+
+            var frameworkProjectDoc = XDocument.Load(frameworkProjectFile);
+            var frameworkProjectCompiles = frameworkProjectDoc.Root.Descendants(projectFileNamespace + "Compile").ToArray();
+            Assert.IsTrue(frameworkProjectCompiles.Any(), "The expected framework Compile elements are not here.");
+
+            var exceptions = coreFiles.Except(frameworkProjectCompiles.Union(framework20ProjectCompiles)
+                .Select(i => i.Attribute("Include").Value.ToLowerInvariant()));
+            if (exceptions.Any())
+            {
+                this.TestContext.WriteLine("The core project has Compile elements that are not in the framework.");
+                this.TestContext.WriteLine($"{exceptions.Count()} exceptions found:");
+                exceptions.OrderBy(i => i).ForEachInEnumerable(i => this.TestContext.WriteLine(i));
+            }
+            else
+            {
+                this.TestContext.WriteLine("No framework-base exceptions found.");
+            }
+
+            this.TestContext.WriteLine(string.Empty);
+
+            exceptions = frameworkProjectCompiles.Union(framework20ProjectCompiles)
+                .Select(i => i.Attribute("Include").Value.ToLowerInvariant()).Except(coreFiles);
+            if (exceptions.Any())
+            {
+                this.TestContext.WriteLine("The framework project has Compile elements that are not in the core.");
+                this.TestContext.WriteLine($"{exceptions.Count()} exceptions found:");
+                exceptions.OrderBy(i => i).ForEachInEnumerable(i => this.TestContext.WriteLine(i));
+            }
+            else
+            {
+                this.TestContext.WriteLine("No base-framework exceptions found.");
+            }
+        }
     }
 }
