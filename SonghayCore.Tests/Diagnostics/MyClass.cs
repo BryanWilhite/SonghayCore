4242397d6ade6590ae00868543d3ad225661944e
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-21 18:58:40 -0800

    :microscope: ShouldGetConfiguredTraceSourceName() passes => issue #13

---
+++ SonghayCore.Tests/Diagnostics/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using Songhay.Diagnostics;
+using Songhay.Extensions;
+using System.Diagnostics;
+
+namespace Songhay.Tests.Diagnostics
+{
+    public class MyClass
+    {
+        static MyClass()
+        {
+            traceSource = TraceSources.Instance.GetTraceSourceFromConfiguredName().WithAllSourceLevels().EnsureTraceSource();
+            traceSource.TraceInformation($"static constructor: {nameof(MyClass)}");
+        }
+
+        static readonly TraceSource traceSource;
+
+        public MyClass()
+        {
+            traceSource.TraceInformation($"{nameof(MyClass)} constructed.");
+        }
+
+        public string GetConfiguredTraceSourceName() => traceSource.Name;
+    }
+}
---
+++ SonghayCore.Tests/Diagnostics/TraceSourcesTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.Diagnostics;
+using Songhay.Models;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Songhay.Tests.Diagnostics
+{
+    [TestClass]
+    public class TraceSourcesTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void ShouldGetConfiguredTraceSourceName()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("./appsettings.json");
+
+            var configuration = builder.Build();
+            var name = configuration[DeploymentEnvironment.DefaultTraceSourceNameConfigurationKey];
+            Assert.IsFalse(string.IsNullOrEmpty(name), "The expected configuration trace source name is not here.");
+            this.TestContext.WriteLine($"configuration trace source name: {name}");
+
+            TraceSources.ConfiguredTraceSourceName = name;
+            Assert.IsTrue(TraceSources.IsConfiguredTraceSourceNameLoaded, $"The expected {nameof(TraceSources)} state is not here.");
+
+            var traceSource = TraceSources.Instance.GetTraceSourceFromConfiguredName();
+
+            using (var listener = new TextWriterTraceListener(Console.Out))
+            {
+                traceSource.Listeners.Add(listener);
+
+                this.TestContext.WriteLine($"instantiating {nameof(MyClass)}...");
+                var mine = new MyClass();
+                Assert.IsTrue(mine.GetConfiguredTraceSourceName() == name, "The expected configured configuration trace source name is not here.");
+            }
+        }
+    }
+}
