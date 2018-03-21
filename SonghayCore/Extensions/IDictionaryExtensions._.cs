09caa3dc42e8857a50323ffd9494c97690f140fe
Bryan Wilhite <rasx@songhaysystem.com>
2018-03-21 14:56:27 -0700

    :pencil: :sparkles: TryGetValueWithKey added

--- SonghayCore/Extensions/IDictionaryExtensions._.cs
+++ SonghayCore/Extensions/IDictionaryExtensions._.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace Songhay.Extensions
@@ -14,16 +15,16 @@ namespace Songhay.Extensions
         /// </summary>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TValue">The type of the value.</typeparam>
-        /// <param name="set">The set.</param>
+        /// <param name="dictionary">The set.</param>
         /// <remarks>
         /// For detail, see https://stackoverflow.com/a/7230446/22944
         /// </remarks>
         /// <returns></returns>
-        public static NameValueCollection ToNameValueCollection<TKey, TValue>(this IDictionary<TKey, TValue> set)
+        public static NameValueCollection ToNameValueCollection<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
         {
             var nameValueCollection = new NameValueCollection();
 
-            foreach (var kvp in set)
+            foreach (var kvp in dictionary)
             {
                 string value = null;
                 if (kvp.Value != null)
@@ -33,6 +34,24 @@ namespace Songhay.Extensions
             }
 
             return nameValueCollection;
+        }
+
+        /// <summary>
+        /// Tries to get value with the specified key.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="dictionary">The dictionary.</param>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">The expected dictionary is not here.</exception>
+        /// <exception cref="System.NullReferenceException"></exception>
+        public static TValue TryGetValueWithKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            if (dictionary == null) throw new ArgumentNullException("The expected dictionary is not here.");
+            if (!dictionary.TryGetValue(key, out var value))
+                throw new NullReferenceException($"The expected value from key, {key}, is not here.");
+            return value;
         }
     }
 }
