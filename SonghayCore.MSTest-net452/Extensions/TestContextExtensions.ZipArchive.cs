9a73d879483c318a4a7aac3680bde655c16144dd
Bryan Wilhite <rasx@songhaysystem.com>
2018-05-09 17:22:10 -0700

    :pencil: :sparkles: added ShouldReadZipArchiveEntriesByLine overloads

--- SonghayCore.MSTest-net452/Extensions/TestContextExtensions.ZipArchive.cs
+++ SonghayCore.MSTest-net452/Extensions/TestContextExtensions.ZipArchive.cs
@@ -53,6 +53,58 @@ namespace Songhay.Extensions
         }
 
         /// <summary>
+        /// Should read zip archive entries by line.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="archiveFile">The archive file.</param>
+        /// <param name="fileAction">The file action.</param>
+        /// <remarks>
+        /// The <c>fileAction</c> includes the line number and the current line.
+        /// </remarks>
+        public static void ShouldReadZipArchiveEntriesByLine(this TestContext context, string archiveFile, Action<int, string> lineAction)
+        {
+            context.ShouldReadZipArchiveEntriesByLine(archiveFile, lineAction, filterZipArchiveEntries: null);
+        }
+
+        /// <summary>
+        /// Should read zip archive entries by line.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="archiveFile">The archive file.</param>
+        /// <param name="fileAction">The file action.</param>
+        /// <param name="filterZipArchiveEntries">The filter zip archive entries.</param>
+        /// <remarks>
+        /// The <c>fileAction</c> includes the line number and the current line.
+        /// </remarks>
+        public static void ShouldReadZipArchiveEntriesByLine(this TestContext context, string archiveFile, Action<int, string> lineAction, Func<ZipArchiveEntry, bool> filterZipArchiveEntries)
+        {
+            context.ShouldFindFile(archiveFile);
+
+            Assert.IsNotNull(lineAction, "The expected ZIP archive file action is not here.");
+
+            if (filterZipArchiveEntries == null) filterZipArchiveEntries = i => true;
+
+            using (var stream = new FileStream(archiveFile, FileMode.Open))
+            using (var zipFile = new ZipArchive(stream, ZipArchiveMode.Read, leaveOpen: false))
+            {
+                zipFile.Entries.Where(filterZipArchiveEntries).ForEachInEnumerable(i =>
+                {
+                    context.WriteLine(i.FullName);
+                    using (var entryStream = new StreamReader(i.Open()))
+                    {
+                        var line = default(string);
+                        var lineNumber = 0;
+                        while ((line = entryStream.ReadLine()) != null)
+                        {
+                            ++lineNumber;
+                            lineAction.Invoke(lineNumber, line);
+                        }
+                    }
+                });
+            }
+        }
+
+        /// <summary>
         /// Should write zip archive entry.
         /// </summary>
         /// <param name="context">The context.</param>
