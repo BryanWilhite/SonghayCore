9b0a7e0f5c52e9de421b96e8f6028d9a1153df35
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-02 18:53:15 -0700

    :hammer:

--- SonghayCore.xUnit/ProjectFileDataAttribute.cs
+++ SonghayCore.xUnit/ProjectFileDataAttribute.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Songhay.Extensions;
 using Songhay.Test.Extensions;
-using Xunit;
 using Xunit.Sdk;
 
 namespace SonghayCore.xUnit
@@ -23,36 +23,6 @@ namespace SonghayCore.xUnit
             _typeInAssembly = typeInAssembly;
             _relativePaths = relativePaths;
             _numbersOfDirectoryLevels = relativePaths.Select(GetNumberOfDirectoryLevels).ToArray();
-        }
-
-        /// <summary>Initializes a new instance of the <see cref="ProjectFileDataAttribute"/> class.</summary>
-        /// <param name="typeInAssembly">The type in assembly to find the project directory.</param>
-        /// <param name="relativePath">The path relative to the project directory.</param>
-        /// <param name="numberOfDirectoryLevels">The number of directory levels above the assembly directory to the project directory.</param>
-        /// <remarks>
-        /// For the modern .NET Core project, <c>numberOfDirectoryLevels = 3</c>: [netcoreapp* or net*]/[Debug or Release]/bin/[project folder]
-        /// For the legacy .NET project, <c>numberOfDirectoryLevels = 2</c>: [Debug or Release]/bin/[project folder]
-        /// </remarks>
-        [Obsolete(@"instead of `numberOfDirectoryLevels` use `..\\..\\` or `../../` prefixes instead")]
-        public ProjectFileDataAttribute(Type typeInAssembly, string relativePath, int numberOfDirectoryLevels)
-        {
-            _typeInAssembly = typeInAssembly;
-            _relativePaths = new[] { relativePath };
-            _numbersOfDirectoryLevels = new[] { numberOfDirectoryLevels };
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ProjectFileDataAttribute"/> class.
-        /// </summary>
-        /// <param name="typeInAssembly">The type in assembly.</param>
-        /// <param name="relativePaths">The relative paths.</param>
-        /// <param name="numberOfDirectoryLevels">The number of directory levels.</param>
-        [Obsolete(@"instead of `numberOfDirectoryLevels` use `..\\..\\` or `../../` prefixes instead")]
-        public ProjectFileDataAttribute(Type typeInAssembly, string[] relativePaths, int numberOfDirectoryLevels)
-        {
-            _typeInAssembly = typeInAssembly;
-            _relativePaths = relativePaths;
-            _numbersOfDirectoryLevels = new[] { numberOfDirectoryLevels };
         }
 
         /// <summary>Returns the data to be used to test the theory.</summary>
@@ -98,4 +68,5 @@ namespace SonghayCore.xUnit
         private readonly Type _typeInAssembly;
         private readonly string[] _relativePaths;
         private readonly int[] _numbersOfDirectoryLevels;
+    }
 }
