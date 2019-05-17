aac23b16c0efa419a7502cc0053469f8f3b3af42
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-05-17 13:22:20 -0700

    :tractor: :sparkles: issue #22

---
+++ SonghayCore/Extensions/ISelectable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Songhay.Extensions
+{
+    /// <summary>
+    /// Defines a selectable visual
+    /// </summary>
+    public interface ISelectable
+    {
+        /// <summary>
+        /// Gets or sets whether this is default selection
+        /// </summary>
+        bool IsDefaultSelection { get; set; }
+
+        /// <summary>
+        ///  Gets or sets whether this is enabled
+        /// </summary>
+        bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether this is selected
+        /// </summary>
+        bool IsSelected { get; set; }
+
+        /// <summary>
+        /// Appends arbitrary values for item selection
+        /// </summary>
+        IDictionary<string, object> Map { get; set; }
+    }
+}
---
+++ SonghayCore/Models/IGroupable.cs
@@ -0,0 +1,23 @@
+﻿namespace Songhay.Models
+{
+    /// <summary>
+    /// Defines a group-able visual
+    /// </summary>
+    public interface IGroupable
+    {
+        /// <summary>
+        /// Display text of the Group
+        /// </summary>
+        string GroupDisplayText { get; set; }
+
+        /// <summary>
+        /// Identifier of the Group
+        /// </summary>
+        string GroupId { get; set; }
+
+        /// <summary>
+        /// Returns `true` when group is visually collapsed
+        /// </summary>
+        bool IsCollapsed { get; set; }
+    }
+}
