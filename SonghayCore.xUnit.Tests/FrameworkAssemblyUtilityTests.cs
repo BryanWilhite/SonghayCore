b4745b5d74c688ccb827b886a6eb9493b8c65dcd
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-06-07 18:43:07 -0700

    :microscope:

--- SonghayCore.xUnit.Tests/FrameworkAssemblyUtilityTests.cs
+++ SonghayCore.xUnit.Tests/FrameworkAssemblyUtilityTests.cs
@@ -1,15 +1,28 @@
 ﻿using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Songhay.Tests
 {
     public class FrameworkAssemblyUtilityTests
     {
-        [Theory, InlineData(@"..\..\..\content\FrameworkAssemblyUtilityTest-ShouldGetPathFromAssembly.json")]
+        public FrameworkAssemblyUtilityTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData(@"..\..\..\content\FrameworkAssemblyUtilityTest-ShouldGetPathFromAssembly.json")]
+        [InlineData("../../../content/FrameworkAssemblyUtilityTest-ShouldGetPathFromAssembly.json")]
+        [InlineData(@"..\..\..\..\SonghayCore\SonghayCore.nuspec")]
+        [InlineData("../../../../SonghayCore/SonghayCore.nuspec")]
         public void GetPathFromAssembly_Test(string fileSegment)
         {
             var actualPath = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, fileSegment);
+            this._testOutputHelper.WriteLine(actualPath);
             Assert.True(File.Exists(actualPath));
         }
+
+        readonly ITestOutputHelper _testOutputHelper;
     }
 }
