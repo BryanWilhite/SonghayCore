0d0e08e022289f5b3c95765e46b532ef49df292a
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-07-11 10:39:25 -0700

    :tractor: :sparkles: resolves #42

---
+++ SonghayCore/Models/HttpMethods.cs
@@ -0,0 +1,50 @@
+﻿#if NETSTANDARD
+using System.Net.Http;
+#endif
+
+namespace Songhay.Models
+{
+    /// <summary>
+    /// Centralizes <see cref="HttpMethod"/> members as strings.
+    /// </summary>
+    /// <remarks>
+    /// Reference: “HTTP request methods” [https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods]
+    /// </remarks>
+    public static class HttpMethods
+    {
+        /// <summary>
+        /// HTTP Method <c>DELETE</c>
+        /// </summary>
+        public const string DELETE = "DELETE";
+
+        /// <summary>
+        /// HTTP Method <c>GET</c>
+        /// </summary>
+        public const string GET = "GET";
+
+        /// <summary>
+        /// HTTP Method <c>HEAD</c>
+        /// </summary>
+        public const string HEAD = "HEAD";
+
+        /// <summary>
+        /// HTTP Method <c>OPTIONS</c>
+        /// </summary>
+        public const string OPTIONS = "OPTIONS";
+
+        /// <summary>
+        /// HTTP Method <c>POST</c>
+        /// </summary>
+        public const string POST = "POST";
+
+        /// <summary>
+        /// HTTP Method <c>PUT</c>
+        /// </summary>
+        public const string PUT = "PUT";
+
+        /// <summary>
+        /// HTTP Method <c>TRACE</c>
+        /// </summary>
+        public const string TRACE = "TRACE";
+    }
+}
