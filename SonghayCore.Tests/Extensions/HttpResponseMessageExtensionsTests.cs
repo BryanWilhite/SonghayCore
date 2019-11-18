99ec23da655b141e7386a5913ba852e85c7f9b11
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-11-18 11:04:15 -0800

    :hammer: :umbrella: resolves #58

---
+++ SonghayCore.Tests/Extensions/HttpResponseMessageExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tavis.UriTemplates;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Songhay.Tests.Extensions
+{
+    public class HttpResponseMessageExtensionsTests
+    {
+        public HttpResponseMessageExtensionsTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Trait("Category", "Integration")]
+        [Theory]
+        [InlineData(@"photos/{photoId}", 1)]
+        public async Task ToJContainerAsync_Test(string input, int id)
+        {
+            var template = new UriTemplate($"{LIVE_API_BASE_URI}/{input}");
+            var uri = template.BindByPosition($"{id}");
+            var jO = JObject.Parse("{\"output\": null}");
+
+            var content = await new HttpRequestMessage(HttpMethod.Get, uri)
+                .GetServerResponseAsync(async response =>
+                {
+                    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                    jO["output"] = await response.ToJContainerAsync();
+                });
+            this._testOutputHelper.WriteLine(jO.ToString());
+        }
+
+        [Trait("Category", "Integration")]
+        [Theory]
+        [InlineData(@"photos")]
+        public async Task ToJContainerAsync_JArray_Test(string input)
+        {
+            var builder = new UriBuilder(LIVE_API_BASE_URI);
+            builder.Path = input;
+            var uri = builder.Uri;
+            var jO = JObject.Parse("{\"output\": null}");
+
+            var content = await new HttpRequestMessage(HttpMethod.Get, uri)
+                .GetServerResponseAsync(async response =>
+                {
+                    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                    jO["output"] = await response.ToJContainerAsync();
+                });
+            this._testOutputHelper.WriteLine(jO.ToString());
+        }
+
+        const string LIVE_API_BASE_URI = "http://jsonplaceholder.typicode.com";
+
+        readonly ITestOutputHelper _testOutputHelper;
+    }
+}
---
+++ SonghayCore/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Songhay.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="HttpResponseMessage"/>.
+    /// </summary>
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Converts the specified <see cref="HttpResponseMessage"/>
+        /// to <see cref="JContainer"/>
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static async Task<JContainer> ToJContainerAsync(this HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            if (string.IsNullOrWhiteSpace(content)) return null;
+
+            return content.TrimStart().StartsWith("[") ?
+                JArray.Parse(content) as JContainer
+                :
+                JObject.Parse(content) as JContainer
+                ;
+        }
+    }
+}
