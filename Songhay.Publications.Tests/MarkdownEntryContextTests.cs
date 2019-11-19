0bdf7fefc269287ec63615c9cbb9b78185e99736
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-11-19 13:03:17 -0800

    resolves #62 and resolves #63 :hammer: :umbrella:

---
+++ Songhay.Publications.Tests/MarkdownEntryContextTests.cs
@@ -0,0 +1,35 @@
+﻿using Songhay.Extensions;
+using Songhay.Tests;
+using System.IO;
+using Xunit;
+
+namespace Songhay.Publications.Tests
+{
+    public class MarkdownEntryContextTests
+    {
+        [DebuggerAttachedTheory]
+        [InlineData("../../../markdown/presentation-drafts", "Hello World!")]
+        public void GenerateEntryFor11ty_Test(string entryRoot, string title)
+        {
+            entryRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, entryRoot);
+
+            var entry = MarkdownEntryContext.GenerateEntryFor11ty(entryRoot, title);
+
+            Assert.NotNull(entry);
+            Assert.True(File.Exists(FrameworkFileUtility.GetCombinedPath(entryRoot, $"{entry.FrontMatter.GetValue<string>("clientId")}.md")));
+        }
+
+        [DebuggerAttachedTheory]
+        [InlineData("../../../markdown/presentation-drafts", "../../../markdown/presentation/entry", "2019-11-19-hello-world.md")]
+        public void PublishEntryFor11ty_Test(string entryRoot, string presentationRoot, string fileName)
+        {
+            entryRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, entryRoot);
+            presentationRoot = FrameworkAssemblyUtility.GetPathFromAssembly(this.GetType().Assembly, presentationRoot);
+
+            var path = MarkdownEntryContext.PublishEntryFor11ty(entryRoot, presentationRoot, fileName);
+
+            Assert.True(File.Exists(path));
+
+        }
+    }
+}
--- Songhay.Publications/MarkdownEntryContext.cs
+++ Songhay.Publications/MarkdownEntryContext.cs
@@ -1,5 +1,7 @@
-﻿using Songhay.Extensions;
+﻿using Newtonsoft.Json.Linq;
+using Songhay.Extensions;
 using Songhay.Publications.Extensions;
+using Songhay.Publications.Models;
 using System;
 using System.IO;
 using System.Linq;
@@ -12,6 +14,41 @@ namespace Songhay.Publications
     public static class MarkdownEntryContext
     {
         /// <summary>
+        /// Generates or overwrites a <see cref="MarkdownEntry"/> file
+        /// at the specified entry root.
+        /// </summary>
+        /// <param name="entryRoot">the conventional directory of <see cref="MarkdownEntry"/> drafts</param>
+        /// <param name="title">see <see cref="MarkdownEntryExtensions.WithNew11tyFrontMatter"/></param>
+        public static MarkdownEntry GenerateEntryFor11ty(string entryRoot, string title)
+        {
+            var tag = JObject.FromObject(new { extract = string.Empty }).ToString();
+            return GenerateEntryFor11ty(entryRoot, title, DateTime.Now, "./entry/", tag);
+        }
+
+        /// <summary>
+        /// Generates or overwrites a <see cref="MarkdownEntry"/> file
+        /// at the specified entry root.
+        /// </summary>
+        /// <param name="entryRoot">the conventional directory of <see cref="MarkdownEntry"/> drafts</param>
+        /// <param name="title">see <see cref="MarkdownEntryExtensions.WithNew11tyFrontMatter"/></param>
+        /// <param name="inceptDate">see <see cref="MarkdownEntryExtensions.WithNew11tyFrontMatter"/></param>
+        /// <param name="path">see <see cref="MarkdownEntryExtensions.WithNew11tyFrontMatter"/></param>
+        /// <param name="tag">see <see cref="MarkdownEntryExtensions.WithNew11tyFrontMatter"/></param>
+        public static MarkdownEntry GenerateEntryFor11ty(string entryRoot, string title, DateTime inceptDate, string path, string tag)
+        {
+            if (!Directory.Exists(entryRoot))
+                throw new DirectoryNotFoundException($"The expected entry root directory, `{entryRoot ?? "[null]"}`, is not here.");
+
+            var entry = new MarkdownEntry()
+                    .WithNew11tyFrontMatter(title, inceptDate, path, tag)
+                    .WithContentHeader();
+
+            File.WriteAllText($"{entryRoot}/{entry.FrontMatter["clientId"]}.md", entry.ToFinalEdit());
+
+            return entry;
+        }
+
+        /// <summary>
         /// Publishes a <see cref="MarkdownEntry"/>
         /// from the specified entry root
         /// to the specified presentation root
@@ -20,9 +57,12 @@ namespace Songhay.Publications
         /// <param name="entryRoot">the conventional directory of <see cref="MarkdownEntry"/> drafts</param>
         /// <param name="presentationRoot">the presentation target directory for publication</param>
         /// <param name="fileName">the name of the <see cref="MarkdownEntry"/> file in the entry directory</param>
-        public static void PublishEntryFor11ty(string entryRoot, string presentationRoot, string fileName)
+        /// <returns>
+        /// Returns the path of the published file.
+        /// </returns>
+        public static string PublishEntryFor11ty(string entryRoot, string presentationRoot, string fileName)
         {
-            PublishEntryFor11ty(entryRoot, presentationRoot, fileName, DateTime.Now);
+            return PublishEntryFor11ty(entryRoot, presentationRoot, fileName, DateTime.Now);
         }
 
         /// <summary>
@@ -35,12 +75,15 @@ namespace Songhay.Publications
         /// <param name="presentationRoot">the presentation target directory for publication</param>
         /// <param name="fileName">the name of the <see cref="MarkdownEntry"/> file in the entry directory</param>
         /// <param name="publicationDate">the <see cref="DateTime"/> of publication</param>
+        /// <returns>
+        /// Returns the path of the published file.
+        /// </returns>
         /// <remarks>
         /// When the publication date is one day later or more than the entry incept date
         /// new eleventy <see cref="MarkdownEntry.FrontMatter"/> will be generated
         /// and the presentation file will be renamed accordingly.
         /// </remarks>
-        public static void PublishEntryFor11ty(string entryRoot, string presentationRoot, string fileName, DateTime publicationDate)
+        public static string PublishEntryFor11ty(string entryRoot, string presentationRoot, string fileName, DateTime publicationDate)
         {
             if (!Directory.Exists(entryRoot))
                 throw new DirectoryNotFoundException($"The expected entry root directory, `{entryRoot ?? "[null]"}`, is not here.");
@@ -69,6 +112,8 @@ namespace Songhay.Publications
             var combinedPath = FrameworkFileUtility.GetCombinedPath(presentationRoot, $"{draftEntry.FrontMatter.GetValue<string>("clientId")}.md");
             File.WriteAllText(combinedPath, draftEntry.ToFinalEdit());
             draftInfo.Delete();
+
+            return combinedPath;
         }
     }
 }
