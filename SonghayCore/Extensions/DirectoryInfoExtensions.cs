aedc6fba3806d6bdcaa64dbc845c1d79b92c81d7
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-12-04 21:22:44 -0800

    :hammer: :pencil2: resolves #77

--- SonghayCore/Extensions/DirectoryInfoExtensions.cs
+++ SonghayCore/Extensions/DirectoryInfoExtensions.cs
@@ -1,11 +1,58 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Songhay.Extensions
 {
     /// <summary>Extensions of <see cref="DirectoryInfo"/>.</summary>
     public static class DirectoryInfoExtensions
     {
+        /// <summary>
+        /// Finds the specified sub <see cref="DirectoryInfo"/>
+        /// under the specified <see cref="DirectoryInfo"/>.
+        /// </summary>
+        /// <param name="directoryInfo">the specified <see cref="DirectoryInfo"/></param>
+        /// <param name="expectedDirectoryName">the specified sub <see cref="DirectoryInfo.Name"/></param>
+        /// <returns></returns>
+        public static DirectoryInfo FindDirectory(this DirectoryInfo directoryInfo, string expectedDirectoryName)
+        {
+            if (directoryInfo == null)
+                throw new DirectoryNotFoundException("The expected directory is not here.");
+
+            if (!directoryInfo.Exists)
+                throw new DirectoryNotFoundException("The expected directory does not exist.");
+
+            var subDirectoryInfo = directoryInfo.GetDirectories(expectedDirectoryName).FirstOrDefault();
+
+            if (subDirectoryInfo == null)
+                throw new DirectoryNotFoundException("The expected directory is not here.");
+
+            return subDirectoryInfo;
+        }
+
+        /// <summary>
+        /// Finds the specified <see cref="FileInfo"/>
+        /// under the specified <see cref="DirectoryInfo"/>.
+        /// </summary>
+        /// <param name="directoryInfo">the specified <see cref="DirectoryInfo"/></param>
+        /// <param name="expectedFileName">the specified <see cref="FileInfo.Name"/></param>
+        /// <returns></returns>
+        public static FileInfo FindFile(this DirectoryInfo directoryInfo, string expectedFileName)
+        {
+            if (directoryInfo == null)
+                throw new DirectoryNotFoundException("The expected directory is not here.");
+
+            if (!directoryInfo.Exists)
+                throw new DirectoryNotFoundException("The expected directory does not exist.");
+
+            var fileInfo = directoryInfo.GetFiles(expectedFileName).FirstOrDefault();
+
+            if (fileInfo == null)
+                throw new FileNotFoundException("The expected file is not here.");
+
+            return fileInfo;
+        }
+
         /// <summary>Gets the parent directory.</summary>
         /// <param name="directoryInfo">The directory information.</param>
         /// <param name="levels">The levels.</param>
@@ -49,6 +96,25 @@ namespace Songhay.Extensions
         {
             if (directoryInfo == null) throw new NullReferenceException($"The expected {nameof(DirectoryInfo)} is not here.");
             return FrameworkFileUtility.GetCombinedPath(directoryInfo.FullName, path);
+        }
+
+        /// <summary>
+        /// Verifies the specified <see cref="DirectoryInfo"/>
+        /// with conventional error handling.
+        /// </summary>
+        /// <param name="directoryInfo">the specified <see cref="DirectoryInfo"/></param>
+        /// <param name="expectedDirectoryName">the expected directory name</param>
+        /// <returns></returns>
+        public static void VerifyDirectory(this DirectoryInfo directoryInfo, string expectedDirectoryName)
+        {
+            if (directoryInfo == null)
+                throw new DirectoryNotFoundException("The expected directory is not here.");
+
+            if (!directoryInfo.Exists)
+                throw new DirectoryNotFoundException("The expected directory does not exist.");
+
+            if (!directoryInfo.Name.EqualsInvariant(expectedDirectoryName))
+                throw new DirectoryNotFoundException($"The expected directory is not here. [actual: { expectedDirectoryName ?? "[name]" }");
         }
     }
 }
