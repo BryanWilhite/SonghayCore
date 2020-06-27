693a073a7cae14de3b20af5f0435324bdbd819e6
Bryan Wilhite <bryan.wilhite@gmail.com>
2020-06-27 15:43:05 -0700

    #89 added `optionalClientGetter` overloads :hammer:

--- SonghayCore/Extensions/HttpRequestMessageExtensions.cs
+++ SonghayCore/Extensions/HttpRequestMessageExtensions.cs
@@ -17,23 +17,41 @@ namespace Songhay.Extensions
         /// <summary>
         /// Gets a <see cref="string"/> from the derived <see cref="HttpResponseMessage"/>.
         /// </summary>
-        /// <param name="request"></param>
+        /// <param name="request">The request.</param>
         public static async Task<string> GetServerResponseAsync(this HttpRequestMessage request)
         {
-            return await request.GetServerResponseAsync(responseMessageAction: null);
+            return await request.GetServerResponseAsync(responseMessageAction: null, optionalClientGetter: null);
         }
 
         /// <summary>
-        /// Gets a <see cref="string"/> from the derived <see cref="HttpResponseMessage"/>.
+        /// Gets a <see cref="string" /> from the derived <see cref="HttpResponseMessage" />.
         /// </summary>
-        /// <param name="request"></param>
-        /// <param name="responseMessageAction"></param>
+        /// <param name="request">The request.</param>
+        /// <param name="responseMessageAction">The response message action.</param>
         /// <returns></returns>
         public static async Task<string> GetServerResponseAsync(this HttpRequestMessage request, Action<HttpResponseMessage> responseMessageAction)
         {
-            if (request == null) throw new NullReferenceException($"The expected {nameof(HttpRequestMessage)} is not here.");
+            return await request.GetServerResponseAsync(responseMessageAction, optionalClientGetter: null);
+        }
 
-            var response = await GetHttpClient().SendAsync(request);
+        /// <summary>
+        /// Gets a <see cref="string" /> from the derived <see cref="HttpResponseMessage" />.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="responseMessageAction">The response message action.</param>
+        /// <param name="optionalClientGetter">The optional client getter.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">request</exception>
+        public static async Task<string> GetServerResponseAsync(this HttpRequestMessage request,
+            Action<HttpResponseMessage> responseMessageAction,
+            Func<HttpClient> optionalClientGetter)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var client = (optionalClientGetter == null) ? GetHttpClient() : optionalClientGetter.Invoke();
+
+            var response = await client.SendAsync(request);
+
             responseMessageAction?.Invoke(response);
 
             var content = await response.Content.ReadAsStringAsync();
@@ -42,30 +60,63 @@ namespace Songhay.Extensions
         }
 
         /// <summary>
-        /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
+        /// Gets any <see cref="Timeout"/> in <see cref="HttpRequestMessage.Properties"/>.
         /// </summary>
-        /// <param name="request"></param>
-        public static async Task<HttpResponseMessage> SendAsync(this HttpRequestMessage request)
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">request</exception>
+        public static TimeSpan? GetTimeout(this HttpRequestMessage request)
         {
-            return await request.SendAsync(requestMessageAction: null);
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (request.Properties.TryGetValue(TIMEOUT_PROPERTY_KEY, out var value) && value is TimeSpan timeout)
+            {
+                return timeout;
+            }
+
+            return null;
         }
 
         /// <summary>
         /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
         /// </summary>
         /// <param name="request"></param>
+        public static async Task<HttpResponseMessage> SendAsync(this HttpRequestMessage request)
+        {
+            return await request.SendAsync(requestMessageAction: null, optionalClientGetter: null);
+        }
+
+        /// <summary>
+        /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
+        /// </summary>
+        /// <param name="request">The request.</param>
         /// <param name="requestMessageAction">The request message action.</param>
+        public static async Task<HttpResponseMessage> SendAsync(this HttpRequestMessage request, Action<HttpRequestMessage> requestMessageAction)
+        {
+            return await request.SendAsync(requestMessageAction, optionalClientGetter: null);
+        }
+
+        /// <summary>
+        /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="requestMessageAction">The request message action.</param>
+        /// <param name="optionalClientGetter">The optional client getter.</param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException">uri - The expected URI is not here.
         /// or
         /// mediaType - The expected request body media type is not here.</exception>
-        public static async Task<HttpResponseMessage> SendAsync(this HttpRequestMessage request, Action<HttpRequestMessage> requestMessageAction)
+        public static async Task<HttpResponseMessage> SendAsync(this HttpRequestMessage request,
+            Action<HttpRequestMessage> requestMessageAction,
+            Func<HttpClient> optionalClientGetter)
         {
-            if (request == null) throw new NullReferenceException($"The expected {nameof(HttpRequestMessage)} is not here.");
+            if (request == null) throw new ArgumentNullException(nameof(request));
 
             requestMessageAction?.Invoke(request);
 
-            var response = await GetHttpClient().SendAsync(request);
+            var client = (optionalClientGetter == null) ? GetHttpClient() : optionalClientGetter.Invoke();
+
+            var response = await client.SendAsync(request);
+
             return response;
         }
 
@@ -74,7 +125,7 @@ namespace Songhay.Extensions
         /// with the specified request body, <see cref="Encoding.UTF8"/>
         /// and <see cref="MimeTypes.ApplicationJson"/>.
         /// </summary>
-        /// <param name="request"></param>
+        /// <param name="request">The request.</param>
         /// <param name="requestBody">The request body.</param>
         public static async Task<HttpResponseMessage> SendBodyAsync(this HttpRequestMessage request, string requestBody)
         {
@@ -85,29 +136,48 @@ namespace Songhay.Extensions
         /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
         /// with the specified request body.
         /// </summary>
-        /// <param name="request"></param>
+        /// <param name="request">The request.</param>
         /// <param name="requestBody">The request body.</param>
         /// <param name="encoding">The encoding.</param>
         /// <param name="mediaType">Type of the media.</param>
         public static async Task<HttpResponseMessage> SendBodyAsync(this HttpRequestMessage request, string requestBody, Encoding encoding, string mediaType)
         {
-            return await request.SendBodyAsync(requestBody, encoding, mediaType, requestMessageAction: null);
+            return await request.SendBodyAsync(requestBody, encoding, mediaType, requestMessageAction: null, optionalClientGetter: null);
         }
 
         /// <summary>
         /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
         /// with the specified request body.
         /// </summary>
-        /// <param name="request"></param>
+        /// <param name="request">The request.</param>
         /// <param name="requestBody">The request body.</param>
         /// <param name="encoding">The encoding.</param>
         /// <param name="mediaType">Type of the media.</param>
         /// <param name="requestMessageAction">The request message action.</param>
         /// <returns></returns>
-        /// <exception cref="ArgumentNullException">requestBody - The expected request body is not here.</exception>
         public static async Task<HttpResponseMessage> SendBodyAsync(this HttpRequestMessage request, string requestBody, Encoding encoding, string mediaType, Action<HttpRequestMessage> requestMessageAction)
         {
-            if (request == null) throw new NullReferenceException($"The expected {nameof(HttpRequestMessage)} is not here.");
+            return await request.SendBodyAsync(requestBody, encoding, mediaType, requestMessageAction, optionalClientGetter: null);
+        }
+
+        /// <summary>
+        /// Calls <see cref="HttpClient.SendAsync(HttpRequestMessage)" />
+        /// with the specified request body.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="requestBody">The request body.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <param name="mediaType">Type of the media.</param>
+        /// <param name="requestMessageAction">The request message action.</param>
+        /// <param name="optionalClientGetter">The optional client getter.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">requestBody - The expected request body is not here.</exception>
+        public static async Task<HttpResponseMessage> SendBodyAsync(this HttpRequestMessage request,
+            string requestBody, Encoding encoding, string mediaType,
+            Action<HttpRequestMessage> requestMessageAction,
+            Func<HttpClient> optionalClientGetter)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
             if (string.IsNullOrWhiteSpace(requestBody)) throw new ArgumentNullException(nameof(requestBody), "The expected request body is not here.");
             if (string.IsNullOrWhiteSpace(mediaType)) throw new ArgumentNullException(nameof(mediaType), "The expected request body media type is not here.");
 
@@ -115,9 +185,25 @@ namespace Songhay.Extensions
 
             requestMessageAction?.Invoke(request);
 
-            var response = await GetHttpClient().SendAsync(request);
+            var client = (optionalClientGetter == null) ? GetHttpClient() : optionalClientGetter.Invoke();
+
+            var response = await client.SendAsync(request);
             return response;
         }
+
+        /// <summary>
+        /// Sets the <see cref="Timeout" /> in <see cref="HttpRequestMessage.Properties" />.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="timeout">The timeout.</param>
+        /// <exception cref="ArgumentNullException">request</exception>
+        public static void SetTimeout(this HttpRequestMessage request, TimeSpan? timeout)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            request.Properties[TIMEOUT_PROPERTY_KEY] = timeout;
+        }
+
+        private const string TIMEOUT_PROPERTY_KEY = "RequestTimeout";
 
         private static HttpClient GetHttpClient() => HttpClientLazy.Value;
 
