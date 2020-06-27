a6951973d74b95b95f3e8dd637d1e6562f0e4bae
Bryan Wilhite <bryan.wilhite@gmail.com>
2020-06-27 16:35:53 -0700

    resolves #89 added `TimeoutHandler` :tractor: :sparkles: :microscope:

---
+++ SonghayCore.Tests/Net/TimeoutHandlerTests.cs
@@ -0,0 +1,87 @@
+﻿using Songhay.Extensions;
+using Songhay.Net;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Songhay.Tests.Net
+{
+    public class TimeoutHandlerTests
+    {
+        public TimeoutHandlerTests(ITestOutputHelper helper)
+        {
+            this._testOutputHelper = helper;
+        }
+
+        [Theory]
+        [InlineData("http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.google.co.uk", 1)]
+        public async Task ShouldCancel(string location, int timeInSeconds)
+        {
+            var handler = new TimeoutHandler
+            {
+                DefaultTimeout = TimeSpan.FromSeconds(10),
+                InnerHandler = new HttpClientHandler()
+            };
+
+            using (var cts = new CancellationTokenSource())
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+                    client.Timeout = Timeout.InfiniteTimeSpan;
+
+                    this._testOutputHelper.WriteLine($"calling `{location}`...");
+                    var request = new HttpRequestMessage(HttpMethod.Get, location);
+
+                    cts.CancelAfter(TimeSpan.FromSeconds(timeInSeconds));
+
+                    using (var response = await client.SendAsync(request, cts.Token))
+                    {
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.IsType<TaskCanceledException>(ex);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData("http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.google.co.uk", 1)]
+        public async Task ShouldTimeout(string location, int timeInSeconds)
+        {
+            var handler = new TimeoutHandler
+            {
+                DefaultTimeout = TimeSpan.FromSeconds(10),
+                InnerHandler = new HttpClientHandler()
+            };
+
+            using (var cts = new CancellationTokenSource())
+            using (var client = new HttpClient(handler))
+            {
+                try
+                {
+                    client.Timeout = Timeout.InfiniteTimeSpan;
+
+                    this._testOutputHelper.WriteLine($"calling `{location}`...");
+                    var request = new HttpRequestMessage(HttpMethod.Get, location);
+
+                    request.SetTimeout(TimeSpan.FromSeconds(timeInSeconds));
+
+                    using (var response = await client.SendAsync(request, cts.Token))
+                    {
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Assert.IsType<TimeoutException>(ex);
+                }
+            }
+        }
+
+        readonly ITestOutputHelper _testOutputHelper;
+    }
+}
---
+++ SonghayCore/Net/TimeoutHandler.cs
@@ -0,0 +1,74 @@
+﻿#if NETSTANDARD
+
+using Songhay.Extensions;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Songhay.Net
+{
+    /// <summary>
+    /// Defines timeout and cancellation support
+    /// for <see cref="HttpClient"/>.
+    /// </summary>
+    /// <seealso cref="DelegatingHandler" />
+    /// <remarks>
+    /// 📖 see “Better timeout handling with HttpClient”
+    /// by @thomaslevesque [ https://github.com/thomaslevesque ]
+    /// [ https://thomaslevesque.com/2018/02/25/better-timeout-handling-with-httpclient/ ]
+    /// 
+    /// </remarks>
+    public class TimeoutHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Gets or sets the default timeout.
+        /// </summary>
+        /// <value>
+        /// The default timeout.
+        /// </value>
+        public TimeSpan DefaultTimeout { get; set; } = TimeSpan.FromSeconds(100);
+
+        /// <summary>
+        /// Sends an HTTP request to the inner handler to send to the server as an asynchronous operation.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send to the server.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="TimeoutException"></exception>
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            using (var cts = GetCancellationTokenSource(request, cancellationToken))
+            {
+                try
+                {
+                    return await base.SendAsync(request, cts?.Token ?? cancellationToken);
+                }
+                catch (OperationCanceledException) when (!cancellationToken.IsCancellationRequested)
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+
+        private CancellationTokenSource GetCancellationTokenSource(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var timeout = request.GetTimeout() ?? DefaultTimeout;
+            if (timeout == Timeout.InfiniteTimeSpan)
+            {
+                // No need to create a CTS if there's no timeout
+                return null;
+            }
+            else
+            {
+                var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                cts.CancelAfter(timeout);
+                return cts;
+            }
+        }
+    }
+}
+
+#endif
