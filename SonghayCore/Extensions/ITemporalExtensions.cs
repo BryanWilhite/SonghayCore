8ebaae0ffcfc31e3ab0e551b17326129560272ff
Bryan Wilhite <rasx@songhaysystem.com>
2021-04-14 17:50:28 -0700

    resolves #110 🖋

--- SonghayCore/Extensions/ITemporalExtensions.cs
+++ SonghayCore/Extensions/ITemporalExtensions.cs
@@ -1,7 +1,6 @@
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Songhay;
-using Songhay.Extensions;
 using Songhay.Models;
 
 namespace Songhay.Extensions
@@ -12,8 +11,33 @@ namespace Songhay.Extensions
     public static class ITemporalExtensions
     {
         /// <summary>
+        /// Sets conventional default values
+        /// for <see cref="ITemporal"/> data.
+        /// </summary>
+        /// <param name="data">the <see cref="ITemporal"/> data</param>
+        public static void SetDefaults(this ITemporal data)
+        {
+            data.SetDefaults(endDate: null);
+        }
+
+        /// <summary>
+        /// Sets conventional default values
+        /// for <see cref="ITemporal"/> data.
+        /// </summary>
+        /// <param name="data">the <see cref="ITemporal"/> data</param>
+        /// <param name="endDate">sets <see cref="ITemporal.EndDate"/></param>
+        public static void SetDefaults(this ITemporal data, DateTime? endDate)
+        {
+            if (data == null) return;
+
+            data.InceptDate = DateTime.Now;
+            data.ModificationDate = data.InceptDate;
+            data.EndDate = endDate;
+        }
+
+        /// <summary>
         /// Converts the specified <see cref="ITemporal"/> data
-        /// to <see cref="JObject"/>
+        /// to <see cref="JObject"/>.
         /// </summary>
         /// <param name="data">the <see cref="ITemporal"/> data</param>
         /// <param name="useJavaScriptCase">when <c>true</c> use snakecase</param>
