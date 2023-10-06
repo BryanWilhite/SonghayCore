c9923e7371f17334707699ea6f292c5ed6311862
Bryan Wilhite <bryan.wilhite@gmail.com>
2023-10-05 23:24:23 -0700

    add `ConvertDateTimeToUtc_Test` 🔬✨

--- SonghayCore.Tests/ProgramTypeUtilityTests.cs
+++ SonghayCore.Tests/ProgramTypeUtilityTests.cs
@@ -1,7 +1,30 @@
-﻿namespace Songhay.Tests;
+﻿using System.Text.Json.Nodes;
+
+namespace Songhay.Tests;
 
 public class ProgramTypeUtilityTests
 {
+
+    [Fact]
+    public void ConvertDateTimeToUtc_Test()
+    {
+        var date = DateTime.Now;
+        var dateString = ProgramTypeUtility.ConvertDateTimeToUtc(date);
+
+        var expected = DateTime.Parse(dateString).ToUniversalTime();
+
+        var json = $"{{ \"one\": {{ \"my-date\": \"{dateString}\" }} }}";
+        var jDoc = JsonDocument.Parse(json);
+        var actual = jDoc.RootElement.GetProperty("one").GetProperty("my-date").GetDateTime();
+
+        Assert.Equal(expected, actual);
+
+        var jO = JsonNode.Parse(json).ToReferenceTypeValueOrThrow();
+        actual = jO["one"]?["my-date"]?.GetValue<DateTime>() ?? DateTime.Now;
+
+        Assert.Equal(expected, actual);
+    }
+
     [Theory]
     [InlineData("Warning", SourceLevels.All, SourceLevels.Warning)]
     [InlineData("Warning,Critical,Error", SourceLevels.All, SourceLevels.All)]
