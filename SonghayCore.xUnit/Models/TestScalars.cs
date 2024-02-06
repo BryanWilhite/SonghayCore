b4502b23d160d4a9c041362e869ce036c1b5a34c
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-02-05 20:44:40 -0800

    resolve #165 🚜✨

---
+++ SonghayCore.xUnit/Models/TestScalars.cs
@@ -0,0 +1,22 @@
+namespace Songhay.Tests.Models;
+
+/// <summary>
+/// Shared values for this assembly.
+/// </summary>
+public static class TestScalars
+{
+    /// <summary>
+    /// The xunit category trait
+    /// </summary>
+    public const string XunitCategory = "Category";
+
+    /// <summary>
+    /// The xunit category integration test
+    /// </summary>
+    public const string XunitCategoryIntegrationTest = "Integration";
+
+    /// <summary>
+    /// The xunit category integration manual test
+    /// </summary>
+    public const string XunitCategoryIntegrationManualTest = "IntegrationManual";
+}
