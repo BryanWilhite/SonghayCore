0c01d60725ccc16ddf7d365a08abbf9c22144d02
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-05-22 11:07:50 -0700

    #169 translate `Songhay.Extensions.ProgramArgsExtensions.GetSettings` to `Songhay.Extensions.IConfigurationExtensions.ReadSettings 🚜✨ translate `Songhay.Extensions.ProgramArgsExtensions.GetSettingsFilePath` to `Songhay.Extensions.IConfigurationExtensions.GetSettingsFilePath 🚜✨  translate `Songhay.Extensions.ProgramArgsExtensions.GetStringInput` to `Songhay.Extensions.IConfigurationExtensions.ReadStringInput 🚜✨  translate `Songhay.Extensions.ProgramArgsExtensions.IsHelpRequest` to `Songhay.Extensions.IConfigurationExtensions.IsHelpRequest 🚜✨ translate `Songhay.Extensions.ProgramArgsExtensions.WriteOutputToFile` overloads to `Songhay.Extensions.IConfigurationExtensions.WriteOutputToFile overloads 🚜✨

--- SonghayCore/Extensions/IConfigurationExtensions.cs
+++ SonghayCore/Extensions/IConfigurationExtensions.cs
@@ -33,9 +33,9 @@ public static class IConfigurationExtensions
     /// when the conventional <see cref="ConsoleArgsScalars.OutputUnderBasePath"/> <c>arg</c> is present.
     /// </summary>
     /// <param name="configuration">the <see cref="IConfiguration"/></param>
-    public static string? GetOutputPath(this IConfiguration? configuration)
+    public static string GetOutputPath(this IConfiguration? configuration)
     {
-        if (configuration == null) return null;
+        if (configuration == null) return string.Empty;
 
         string outputFile = configuration[ConsoleArgsScalars.OutputFile.ToConfigurationKey()];
 
@@ -54,6 +54,14 @@ public static class IConfigurationExtensions
     }
 
     /// <summary>
+    /// Returns the value of the conventional <see cref="ConsoleArgsScalars.SettingsFile"/> <c>arg</c>
+    /// which should be a path to a text file.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    public static string? GetSettingsFilePath(this IConfiguration? configuration) =>
+        configuration?[ConsoleArgsScalars.SettingsFile.WithConfigurationHelpTextSuffix()];
+
+    /// <summary>
     /// Returns <c>true</c> when the specified <see cref="IConfiguration"/>
     /// contains the specified key.
     /// </summary>
@@ -65,6 +73,68 @@ public static class IConfigurationExtensions
     /// </remarks>
     public static bool HasArg(this IConfiguration? configuration, string? key) =>
         configuration.ToKeys().Contains(key.ToConfigurationKey());
+
+    /// <summary>
+    /// Determines whether <c>args</c> contain the convnetional <see cref="ProgramArgs.Help"/> flag.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    public static bool IsHelpRequest(this IConfiguration? configuration) =>
+        configuration.HasArg(ConsoleArgsScalars.Help.ToConfigurationKey());
+
+    /// <summary>
+    /// Reads the settings data in the file specified by <see cref="ConsoleArgsScalars.SettingsFile"/>
+    /// which could be a relative or absolute path.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    public static string ReadSettings(this IConfiguration? configuration)
+    {
+        string? path = configuration.GetSettingsFilePath();
+
+        if (File.Exists(path)) return File.ReadAllText(path);
+
+        string? basePath = configuration.GetBasePathValue();
+
+        path = ProgramFileUtility.GetCombinedPath(basePath, path, fileIsExpected: true);
+
+        return File.ReadAllText(path);
+    }
+
+    /// <summary>
+    /// Reads the input data in the inline <see cref="string"/>
+    /// specified by <see cref="ConsoleArgsScalars.InputString"/>
+    /// or the file specified by <see cref="ConsoleArgsScalars.InputFile"/>
+    /// which could be a relative or absolute path.
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <exception cref="FileNotFoundException"></exception>
+    public static string? ReadStringInput(this IConfiguration? configuration)
+    {
+        string? input = null;
+
+        if (configuration.HasArg(ConsoleArgsScalars.InputString))
+        {
+            input = configuration?[ConsoleArgsScalars.InputString.ToConfigurationKey()];
+        }
+        else if (configuration.HasArg(ConsoleArgsScalars.InputFile))
+        {
+            var path = configuration?[ConsoleArgsScalars.InputFile.ToConfigurationKey()];
+
+            if (!File.Exists(path))
+            {
+                var basePath = configuration.GetBasePathValue();
+
+                path = ProgramFileUtility
+                    .GetCombinedPath(basePath, path, fileIsExpected: true);
+
+                if (!File.Exists(path))
+                    throw new FileNotFoundException($"The expected input file, `{path}`, is not here.");
+            }
+
+            input = File.ReadAllText(path);
+        }
+
+        return input;
+    }
 
     /// <summary>
     /// Converts the specified <see cref="IConfiguration"/>
@@ -111,5 +181,95 @@ public static class IConfigurationExtensions
             $"The path to the file to load as Activity Settings input. This can be an absolute path or relative to {ConsoleArgsScalars.BasePath}.";
 
         return configuration;
+    }
+
+    /// <summary>
+    /// Writes the <see cref="string"/> output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    public static void WriteOutputToFile(this IConfiguration? configuration, string output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        File.WriteAllText(outputFile, output);
+    }
+
+    /// <summary>
+    /// Writes the <see cref="string"/> output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    public static async Task WriteOutputToFileAsync(this IConfiguration? configuration, string output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        await File.WriteAllTextAsync(outputFile, output);
+    }
+
+    /// <summary>
+    /// Writes the <see cref="byte"/> array output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    public static void WriteOutputToFile(this IConfiguration? configuration, byte[] output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        File.WriteAllBytes(outputFile, output);
+    }
+
+    /// <summary>
+    /// Writes the <see cref="byte"/> array output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    public static async Task WriteOutputToFileAsync(this IConfiguration? configuration, byte[] output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        await File.WriteAllBytesAsync(outputFile, output);
+    }
+
+    /// <summary>
+    /// Writes the <see cref="Stream"/> output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    /// <remarks>
+    /// [ see https://stackoverflow.com/a/5515894/22944 ]
+    /// </remarks>
+    public static void WriteOutputToFile(this IConfiguration? configuration, Stream output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        using var fileStream = File.Create(outputFile);
+
+        output.Seek(0, SeekOrigin.Begin);
+        output.CopyTo(fileStream);
+    }
+
+    /// <summary>
+    /// Writes the <see cref="Stream"/> output
+    /// to the file specified by <see cref="ConsoleArgsScalars.OutputFile"/>.
+    /// </summary>
+    /// <param name="configuration">the <see cref="IConfiguration"/></param>
+    /// <param name="output">the output to write</param>
+    /// <remarks>
+    /// [ see https://stackoverflow.com/a/5515894/22944 ]
+    /// </remarks>
+    public static async Task WriteOutputToFileAsync(this IConfiguration? configuration, Stream output)
+    {
+        var outputFile = configuration.GetOutputPath();
+
+        using var fileStream = File.Create(outputFile);
+
+        output.Seek(0, SeekOrigin.Begin);
+        await output.CopyToAsync(fileStream);
     }
 }
