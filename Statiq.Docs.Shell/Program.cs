2ce9f67c59cfa88c2610b7bf2d295044217e468f
Bryan Wilhite <bryan.wilhite@gmail.com>
2025-01-24 22:59:07 -0800

    🔨

--- Statiq.Docs.Shell/Program.cs
+++ Statiq.Docs.Shell/Program.cs
@@ -1,25 +1,26 @@
 ﻿await Bootstrapper
     .Factory
     .CreateDocs(args)
-    .AddExcludedPath("../../../../Statiq.Docs.Shell")
+    .AddExcludedPath("Statiq.Docs.Shell")
+    .AddAssemblyFiles("SonghayCore/**/*.dll")
     .AddSettings(new Dictionary<string, object>
     {
         { Keys.Title, "API" },
         { Keys.LinkRoot, "SonghayCore" },
         { DocsKeys.ApiPath, "latest" },
-        {
-            DocsKeys.SourceFiles,
-            new []
-            {
-                "../../../../../SonghayCore/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
-                "../../../../../SonghayCore.xUnit/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
-            }
-        },
+        // {
+        //     DocsKeys.SourceFiles,
+        //     new []
+        //     {
+        //         "SonghayCore/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
+        //         "SonghayCore.xUnit/**/{!.git,!bin,!obj,!packages,!*.Tests,}/**/*.cs",
+        //     }
+        // },
         { DocsKeys.OutputApiDocuments, true },
         { WebKeys.Author, "@BryanWilhite" },
         { WebKeys.Copyright, $"(c) {DateTime.Now.Year} Songhay System" },
         { WebKeys.GitHubName, "Bryan Wilhite" },
         { WebKeys.GitHubUsername, "BryanWilhite" },
-        { WebKeys.OutputPath, "../../../../docs" },
+        //{ WebKeys.OutputPath, "docs" },
     })
     .RunAsync();
