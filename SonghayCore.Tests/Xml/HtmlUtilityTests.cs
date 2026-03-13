0c0b4a7985ee11ee1043a00b5850dd8e10438d59
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-03-13 12:43:06 -0700

    #93 🚜✨ 🔬✨

--- SonghayCore.Tests/Xml/HtmlUtilityTests.cs
+++ SonghayCore.Tests/Xml/HtmlUtilityTests.cs
@@ -21,6 +21,60 @@ public class HtmlUtilityTests
 
     [InlineData("<option selected=\"selected\">", "<option selected>")]
     [InlineData("<option value=\"dog\" selected=\"selected\">Dog</option>", "<option value=\"dog\" selected>Dog</option>")]
+
+    [InlineData(
+        """
+        <html lang="en">
+            <head>
+                <meta charset='utf-8' />
+                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+                <meta name='viewport' content='width=device-width, initial-scale=1'></meta>
+                <link rel='stylesheet' type='text/css' media='screen' href='_bundles/styles.min.css'></link>
+                <link rel="SHORTCUT ICON" href="images/favicon.ico" />
+
+                <script src='_bundles/scripts.min.js'></script>
+
+                <title>Demo</title>
+            </head>
+
+            <body>
+                <select>
+                    <option selected="selected">Dog</option>
+                    <option value="cat">Cat</option>
+                </select>
+                <select>
+                    <option value="dog" selected="selected">Dog</option>
+                    <option value="cat">Cat</option>
+                </select>
+            </body>
+        </html>
+        """,
+    """
+        <html>
+            <head>
+                <meta charset='utf-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                <link rel='stylesheet' type='text/css' media='screen' href='_bundles/styles.min.css'>
+                <link rel="SHORTCUT ICON" href="images/favicon.ico">
+
+                <script src='_bundles/scripts.min.js'></script>
+
+                <title>Demo</title>
+            </head>
+
+            <body>
+                <select>
+                    <option selected>Dog</option>
+                    <option value="cat">Cat</option>
+                </select>
+                <select>
+                    <option value="dog" selected>Dog</option>
+                    <option value="cat">Cat</option>
+                </select>
+            </body>
+        </html>
+        """)]
     public void ConvertToHtml_Test(string input, string expected)
     {
         string? actual = HtmlUtility.ConvertToHtml(input);
