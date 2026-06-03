d801133e4370824d66136efdc58299befa17adfc
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-06-03 12:39:23 -0700

    `ToIso8601String` should be marked obsolete 🔨🧊

--- SonghayCore.Tests/Extensions/DateTimeExtensionsTests.cs
+++ SonghayCore.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -1,8 +1,9 @@
+using System.Globalization;
 using System.Text.Json.Nodes;
 
 namespace Songhay.Tests.Extensions;
 
-public class DateTimeExtensionsTests
+public class DateTimeExtensionsTests(ITestOutputHelper testOutputHelper)
 {
     [Theory]
     [InlineData("Monday, January 15, 2024", DayOfWeek.Wednesday, "Wednesday, January 17, 2024")]
@@ -16,20 +17,68 @@ public class DateTimeExtensionsTests
         Assert.Equal(expected, actual);
     }
 
+    /// <remarks>
+    /// This impoverished test verifies that I am not the inventor of Noda Time.
+    /// The official docs clearly state that <see cref="DateTime.Parse(string)"/>
+    /// will default to the current culture.
+    /// (See https://learn.microsoft.com/en-us/dotnet/api/system.datetime.parse?view=net-10.0)
+    /// </remarks>
+    [Theory]
+    [InlineData("2026-06-03T19:00:00Z")]
+    public void ShouldConvertIso8601UtcStringToLocal(string input)
+    {
+        DateTime actual = DateTime.Parse(input);
+        DateTime expected = actual.ToUniversalTime();
+
+        testOutputHelper.WriteLine($"input: `{input}`");
+        testOutputHelper.WriteLine($"current-culture offset: {DateTimeOffset.Now.Offset.Hours}");
+        testOutputHelper.WriteLine($"actual hour: {actual.Hour}");
+        testOutputHelper.WriteLine($"actual kind: {actual.Kind}");
+        testOutputHelper.WriteLine($"expected hour: {expected.Hour}");
+        testOutputHelper.WriteLine($"expected kind: {expected.Kind}");
+
+        Assert.NotEqual(expected, actual);
+    }
+
+    /// <remarks>
+    /// This test shows how to parse an ISO 8601 UTC string
+    /// under <see cref="CultureInfo.InvariantCulture"/>
+    /// while preserving UTC time.
+    /// This test also shows that calling <see cref="DateTime.ToUniversalTime"/>
+    /// on a value that is already <see cref="DateTimeKind.Utc"/> will not erroneously convert it again.
+    /// </remarks>
+    [Theory]
+    [InlineData("2026-06-03T19:00:00Z")]
+    public void ShouldConvertIso8601UtcString(string input)
+    {
+        DateTime actual = DateTime.Parse(input, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+        DateTime expected = actual.ToUniversalTime();
+
+        testOutputHelper.WriteLine($"input: `{input}`");
+        testOutputHelper.WriteLine($"current-culture offset: {DateTimeOffset.Now.Offset.Hours}");
+        testOutputHelper.WriteLine($"actual hour: {actual.Hour}");
+        testOutputHelper.WriteLine($"actual kind: {actual.Kind}");
+        testOutputHelper.WriteLine($"expected hour: {expected.Hour}");
+        testOutputHelper.WriteLine($"expected kind: {expected.Kind}");
+
+        Assert.Equal(expected, actual);
+    }
+
     [Fact]
     public void ToIso8601String_Test()
     {
-        var dateString = DateTime.Now.ToIso8601String();
+        string dateString = DateTime.Now.ToIso8601String();
+        testOutputHelper.WriteLine(dateString);
 
-        var expected = DateTime.Parse(dateString).ToUniversalTime();
+        DateTime expected = DateTime.Parse(dateString).ToUniversalTime();
 
-        var json = $"{{ \"one\": {{ \"my-date\": \"{dateString}\" }} }}";
-        using var jDoc = JsonDocument.Parse(json);
-        var actual = jDoc.RootElement.GetProperty("one").GetProperty("my-date").GetDateTime();
+        string json = $"{{ \"one\": {{ \"my-date\": \"{dateString}\" }} }}";
+        using JsonDocument jDoc = JsonDocument.Parse(json);
+        DateTime actual = jDoc.RootElement.GetProperty("one").GetProperty("my-date").GetDateTime();
 
         Assert.Equal(expected, actual);
 
-        var jO = JsonNode.Parse(json).ToReferenceTypeValueOrThrow();
+        JsonNode jO = JsonNode.Parse(json).ToReferenceTypeValueOrThrow();
         actual = jO["one"]?["my-date"]?.GetValue<DateTime>() ?? DateTime.Now;
 
         Assert.Equal(expected, actual);
