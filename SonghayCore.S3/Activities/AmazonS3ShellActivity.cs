4e457ad282540c578c2a7679bc4820d94bac483c
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-08-28 13:12:09 -0700

    add SonghayCore.S3.Activities.AmazonS3*Activity for CLI and BackgroundService 🚜✨

---
+++ SonghayCore.S3/Activities/AmazonS3ShellActivity.cs
@@ -0,0 +1,107 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using S3ShellInput =
+(
+    string setKey,
+    string bucketMetaKey,
+    string? bucketKey,
+    string? content,
+    string? contentMimeType
+);
+
+namespace SonghayCore.S3.Activities;
+
+public class AmazonS3ShellActivity(IConfiguration? configuration,
+        [FromKeyedServices(nameof(AmazonS3DeleteS3ObjectActivity))] IActivityTask<(string setKey, string bucketMetaKey, string bucketKey)> activityForAmazonS3DeleteS3Object,
+        [FromKeyedServices(nameof(AmazonS3DownloadToStringActivity))] IActivityTask<(string setKey, string bucketMetaKey, string bucketKey), string?> activityForAmazonS3DownloadToString,
+        [FromKeyedServices(nameof(AmazonS3ListBucketObjectsWithPaginationActivity))] IActivityTask<(string setKey, string bucketMetaKey), string?> activityForAmazonS3ListBucketObjectsWithPagination,
+        [FromKeyedServices(nameof(AmazonS3UploadStringActivity))] IActivityTask<(string setKey, string bucketMetaKey, string bucketKey, string content, string contentMimeType)> activityForAmazonS3UploadString,
+        ILogger<AmazonS3ShellActivity>logger
+
+    ) : IActivityOutputOnlyTask<string?>
+{
+    public async Task<string?> StartAsync()
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+        ILoggerUtility.AsInstanceOrNullLogger(logger);
+
+        logger.LogInformation("{ActivityName} starting...", nameof
+        (AmazonS3ShellActivity));
+
+        string? setKey = configuration.GetCommandLineArgValue(ArgSetKey);
+        setKey.ThrowWhenNullOrWhiteSpace();
+
+        string? bucketMetaKey = configuration.GetCommandLineArgValue(ArgBucketMetaKey);
+        bucketMetaKey.ThrowWhenNullOrWhiteSpace();
+
+        string? bucketKey = configuration.GetCommandLineArgValue(ArgBucketKey);
+        string? content = configuration.ReadStringInput();
+        string? contentMimeType = configuration.GetCommandLineArgValue(ArgBucketS3ObjectMimetype);
+
+        S3ShellInput input = (setKey, bucketMetaKey, bucketKey, content, contentMimeType);
+
+        string? activitySetKey = configuration.GetCommandLineArgValue(ConsoleArgsScalars.ActivityName);
+        activitySetKey.ThrowWhenNullOrWhiteSpace();
+
+        Func<S3ShellInput, Task<string?>>? activity = ActivitySet.TryGetValueWithKey(activitySetKey);
+
+        if (activity == null)
+        {
+            logger.LogError("The expected Activity, `{Name}`, is not here.", activitySetKey);
+
+            return null;
+        }
+
+        return await activity.Invoke(input);
+    }
+
+    internal const string ArgSetKey = "--set-key";
+    internal const string ArgBucketMetaKey = "--bucket-meta-key";
+    internal const string ArgBucketKey = "--bucket-key";
+    internal const string ArgBucketS3ObjectMimetype = "--bucket-object-mime-type";
+
+    internal readonly Dictionary<string, Func<S3ShellInput, Task<string?>>> ActivitySet = new()
+    {
+        [nameof(AmazonS3DeleteS3ObjectActivity)] = async input =>
+        {
+            var (setKey, bucketMetaKey, bucketKey, _, _) = input;
+
+            bucketKey.ThrowWhenNullOrWhiteSpace();
+
+            await activityForAmazonS3DeleteS3Object.StartAsync((setKey, bucketMetaKey, bucketKey));
+
+            return null;
+        },
+        [nameof(AmazonS3DownloadToStringActivity)] = async input =>
+        {
+            var (setKey, bucketMetaKey, bucketKey, _, _) = input;
+
+            bucketKey.ThrowWhenNullOrWhiteSpace();
+
+            string? output = await activityForAmazonS3DownloadToString.StartAsync((setKey, bucketMetaKey, bucketKey));
+
+            return output;
+        },
+        [nameof(AmazonS3ListBucketObjectsWithPaginationActivity)] = async input =>
+        {
+            var (setKey, bucketMetaKey, _, _, _) = input;
+
+            string? output = await activityForAmazonS3ListBucketObjectsWithPagination.StartAsync((setKey, bucketMetaKey));
+
+            return output;
+        },
+        [nameof(AmazonS3UploadStringActivity)] = async input =>
+        {
+            var (setKey, bucketMetaKey, bucketKey, content, contentMimeType) = input;
+
+            bucketKey.ThrowWhenNullOrWhiteSpace();
+            content.ThrowWhenNullOrWhiteSpace();
+            contentMimeType.ThrowWhenNullOrWhiteSpace();
+
+            await activityForAmazonS3UploadString.StartAsync((setKey, bucketMetaKey, bucketKey, content, contentMimeType));
+
+            return null;
+        },
+    };
+}
