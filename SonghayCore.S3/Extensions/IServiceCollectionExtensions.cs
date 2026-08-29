b9858511062c42c169cfbff1e5145f8a091b2847
Bryan Wilhite <bryan.wilhite@gmail.com>
2026-08-28 21:38:23 -0700

    add `IServiceCollectionExtensions` issue #197 🚜✨

---
+++ SonghayCore.S3/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using SonghayCore.S3.Activities;
+using SonghayCore.S3.Hosting;
+
+namespace SonghayCore.S3.Extensions;
+
+/// <summary>
+/// Extensions of <see cref="IServiceCollection"/>
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddS3HostedService<THostedService>(this IServiceCollection services) where THostedService : class, IHostedService
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services
+            .AddS3HostedServiceDependencies<THostedService>()
+            .AddHostedService<THostedService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddS3HostedServiceDependencies<THostedService>(this IServiceCollection services) where THostedService : class, IHostedService
+    {
+        string procedureName = typeof(THostedService).Name;
+
+        switch (procedureName)
+        {
+            case nameof(AmazonS3Service):
+                services
+                    .AddKeyedTransient<IActivityTask<(string setKey, string bucketMetaKey, string bucketKey)>, AmazonS3DeleteS3ObjectActivity>(nameof(AmazonS3DeleteS3ObjectActivity))
+                    .AddKeyedTransient<IActivityTask<(string setKey, string bucketMetaKey, string bucketKey), string?>, AmazonS3DownloadToStringActivity>(nameof(AmazonS3DownloadToStringActivity))
+                    .AddKeyedTransient<IActivityTask<(string setKey, string bucketMetaKey), string?>, AmazonS3ListBucketObjectsWithPaginationActivity>(nameof(AmazonS3ListBucketObjectsWithPaginationActivity))
+                    .AddKeyedTransient<IActivityTask<(string setKey, string bucketMetaKey, string bucketKey, string content, string contentMimeType)>, AmazonS3UploadStringActivity>(nameof(AmazonS3UploadStringActivity));
+
+                break;
+        }
+
+        return services;
+    }
+}
